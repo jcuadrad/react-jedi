8e79139573a7391cad1e2488201d1f088c44bbc7
Juan Cuadra <jcb8793@gmail.com>
2018-01-02 02:25:02 -0600

    Comments

--- src/Race.js
+++ src/Race.js
@@ -11,12 +11,16 @@ export default class Race {
     }
   
     getTime(spaceship) {
+      //Calculate the number of trips for each spaceship, both with or without cargo.
+      //Substract one because there's no last trip back
       let tripsWithCargo = Math.ceil(this.gold/spaceship.cargo);
       let tripsWithoutCargo = tripsWithCargo - 1;
-  
+
+      // Calculate the time of the trips, adding the hours needed for loading.
       let timeWithCargo = ((this.distance/spaceship.speed) + 2) * tripsWithCargo;
       let timeWithoutCargo = (this.distance/spaceship.speed) * tripsWithoutCargo;
   
+      //Return the final sum of time
       return parseFloat((timeWithCargo + timeWithoutCargo).toFixed(4));
     }
 };
